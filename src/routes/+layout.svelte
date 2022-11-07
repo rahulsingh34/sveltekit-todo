53a3d62ccfec35405be3b80aa4b110e3ed528aa3
rahulsingh34 <rahulsingh342000@gmail.com>
2022-11-06 19:12:40 -0500

    database hooked up to controllers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script>
 	import '../app.postcss';
     import { supabase } from "../supabase"
-    console.log(supabase)
 </script>
 
 <div class="container mx-auto my-6 max-w-lg">
--- src/stores/todoStores.js
+++ src/stores/todoStores.js
@@ -1,19 +1,38 @@
 import { writable } from "svelte/store";
-
+import { supabase } from "../supabase";
 export const todos = writable([]);
 
-export const addTodo = (text) => {
-    todos.update((cur) => {
-        const newTodos = [...cur, {text, completed: false, id:Date.now()}]
-        return newTodos
-    })
+export const loadTodos = async () => {
+    const {data, error} = await supabase.from('todos').select()
+    if(error) {
+        return console.error(error)
+    }
+    todos.set(data)
+}
+loadTodos()
+
+export const addTodo = async (text, user_id) => {
+    const {data, error} = await supabase.from('todos').insert([{text, user_id}]);
+    if(error) {
+        return console.error(error);
+    }
+    loadTodos()
 }
 
-export const deleteTodo = (id) => {
+export const deleteTodo = async (id) => {
+    const {error} = await supabase.from('todos').delete().match({id})
+    if(error) {
+        return console.error(error)
+    }
     todos.update((todos) => todos.filter((todo) => todo.id != id))
 }
 
-export const toggleTodoCompleted = (id) => {
+export const toggleTodoCompleted = async (id, currentState) => {
+    const {error} = await supabase.from('todos').update({completed: !currentState}).match({id})
+    if(error) {
+        return console.error(error)
+    }
+
     todos.update((todos) => {
         let index = -1
         for(let i = 0; i < todos.length; i++) {
